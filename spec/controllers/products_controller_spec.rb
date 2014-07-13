60aba5ed4613eb7b4bd2b5076d7fa56eaa0c963b
jksun <jksun@thoughtworks.com>
2014-07-13 21:41:09 +0800

    return uri and product info when get

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, :type => :controller do
   render_views
-  describe "GET" do
+  fixtures(:all)
+
+  describe "GET Index" do
     context "product exist" do
 
       before(:each) {
-        expect(Product).to receive(:all).and_return([Product.new])
+        expect(Product).to receive(:all).and_return(products(:apple))
         get :index, format: :json
         @json = JSON.parse(response.body)
       }
@@ -17,6 +19,18 @@ RSpec.describe ProductsController, :type => :controller do
 
       it 'have one product' do
         expect(@json.length).to eq(1)
+      end
+
+      it 'product have name' do
+        expect(@json[0]['name']).to eq("apple")
+      end
+
+      it 'product have description' do
+        expect(@json[0]['description']).to eq(products(:apple).description)
+      end
+
+      it 'product have uri' do
+        expect(@json[0]['uri']).to end_with("/products/#{products(:apple).id}")
       end
     end
   end
